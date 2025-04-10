0a8c857c45bdf22ee3972b6f7926942243f710ee
Matias Petenatti <matiasodin96@gmail.com>
2025-04-10 11:15:10 -0300

    finger 5 and 6

--- Lecture 5/finger-5.py
+++ Lecture 5/finger-5.py
@@ -2,3 +2,9 @@
 # Write a piece of Python code that prints out a new string containing the even indexed characters of my_str. 
 # For example, if my_str = "abcdefg" then your code should print out aceg.
 
+my_str = "abcdefg"
+
+evenChars = ''
+for i in range(0,len(my_str),2):
+    evenChars += my_str[i]
+print(evenChars)
--- Lecture 6/finger-6.py
+++ Lecture 6/finger-6.py
@@ -2,3 +2,22 @@
 # Write a piece of Python code that uses bisection search to guess N. 
 # The code prints two lines: count: with how many guesses it took to find N, and answer: with the value of N. 
 # Hints: If the halfway value is exactly in between two integers, choose the smaller one.
+
+N = 35
+
+count = 0
+low = 0
+high = 1000
+
+response = (high + low) / 2
+
+while abs(response - N) >= 0.01:
+    if response < N:
+        low = response
+    else:
+        high = response
+    response = (high + low) / 2
+    count += 1
+
+print(count)
+print(response)

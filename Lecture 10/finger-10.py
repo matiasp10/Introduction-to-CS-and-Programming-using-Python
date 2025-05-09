f0b608490bb48b37d6540e9ccb971a37c334ab36
Matias Petenatti <matiasodin96@gmail.com>
2025-05-09 14:42:15 -0300

    finger 10-11

---
+++ Lecture 10/finger-10.py
@@ -0,0 +1,17 @@
+# Finger Exercises Lecture 9
+# Name: Matias Ezequiel Petenatti
+# Time Spent: 0:15
+
+def all_true(n, Lf):
+    """ n is an int
+        Lf is a list of functions that take in an int and return a Boolean
+    Returns True if each and every function in Lf returns True when called 
+    with n as a parameter. Otherwise returns False. 
+    """
+    for f in Lf:
+        if not f(n):
+            return False
+    return True
+
+# Examples:    
+all_true() # prints 6

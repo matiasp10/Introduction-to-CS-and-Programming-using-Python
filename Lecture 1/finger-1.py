ba6c62c03f300e1ba3fca589da50690fd51d49c6
Matias Petenatti <matiasodin96@gmail.com>
2025-05-24 20:06:07 -0300

    update lecture 1

--- Lecture 1/finger-1.py
+++ Lecture 1/finger-1.py
@@ -1,6 +1,6 @@
 # Finger Exercises Lecture 1
 # Name: Matias Ezequiel Petenatti
-# Time Spent: 0:15
+# Time Spent: 0:5
 
 """
 Assume 3 variables are already defined for you: a, b, and c. Create a variable called total that adds a and b then multiplies the result by c. Include a last line in your code to print the value: print(total)

a3989fcb1177ff57fc10af706aab4efacffd3d2d
Matias Petenatti <matiasodin96@gmail.com>
2025-06-20 20:00:00 -0300

    feat: Añadido Lectura 15

---
+++ Lecture-15/finger-15.py
@@ -0,0 +1,20 @@
+# Finger Exercises Lecture 15
+# Name: Matias Ezequiel Petenatti
+# Time Spent: 0:15
+
+def recur_power(base, exp):
+    """
+    base: int or float.
+    exp: int >= 0
+
+    Returns base to the power of exp using recursion.
+    Hint: Base case is when exp = 0. Otherwise, in the recursive
+    case you return base * base^(exp-1).
+    """
+    if exp == 0:
+        return 1
+    else:
+        return base * recur_power(base, exp - 1) 
+
+# Examples:
+print(recur_power(2,5))  # prints 32
